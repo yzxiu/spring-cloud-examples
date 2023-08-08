d69c4581f4306767682751de68426ec3389151ed
yaozhenxiu <946666800@qq.com>
2023-08-08 15:32:55 +0800

    update

--- eureka-producer-consumer/spring-cloud-producer/src/main/java/com/neo/controller/HelloController.java
+++ eureka-producer-consumer/spring-cloud-producer/src/main/java/com/neo/controller/HelloController.java
@@ -4,11 +4,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloController {
-	
+
+    InetAddress ia;
+
+    {
+        try {
+            ia = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    String host = ia.getHostName();
+
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
-        return "hello "+name+"，this is first messge";
+        return "hello " + name + "，this is " + host;
     }
 }
--- spring-cloud-consul/spring-cloud-consul-producer/src/main/java/com/neo/consul/web/HelloController.java
+++ spring-cloud-consul/spring-cloud-consul-producer/src/main/java/com/neo/consul/web/HelloController.java
@@ -3,11 +3,26 @@ package com.neo.consul.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "helle consul";
+
+        InetAddress ia;
+
+        {
+            try {
+                ia = InetAddress.getLocalHost();
+            } catch (UnknownHostException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        String host = ia.getHostName();
+        return "helle consul from " + host;
     }
 }
